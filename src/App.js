43d4836e61bd0d4aebae966e808c7042e6a4833d
gabrielpasquantonio <64622616+gabrielpasquantonio@users.noreply.github.com>
2021-05-06 21:51:51 -0300

    test

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Login from "./components/Login"
+import './App.css';
+
+function App() {
+  return (
+ <Router>
+<Switch>
+<Route exact path="/">
+<Login/>
+</Route>
+</Switch>
+</Router>
+  );
+}
+
+export default App;
