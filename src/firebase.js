a73c9feb3e05e68c8f8dc91aa05b1c30a7cabe73
gabrielpasquantonio <64622616+gabrielpasquantonio@users.noreply.github.com>
2021-05-16 14:25:53 -0300

    adding files to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,15 @@
 import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyCbxRpjno7lkXoVIXLHy4h3bvyDU_xIELA",
-    authDomain: "disneyplus-clone-79841.firebaseapp.com",
-    databaseURL: "https://disneyplus-clone-79841-default-rtdb.firebaseio.com",
-    projectId: "disneyplus-clone-79841",
-    storageBucket: "disneyplus-clone-79841.appspot.com",
-    messagingSenderId: "249201755674",
-    appId: "1:249201755674:web:5bb9fed4fbbb58ef2df3a3",
-    measurementId: "G-S479KYDXXW"
+const  firebaseConfig = {
+    apiKey: "AIzaSyDFxslAjuA1ajPOEUWssDezr_qBQ2SxsC0",
+    authDomain: "disney-plus-5b92e.firebaseapp.com",
+    projectId: "disney-plus-5b92e",
+    storageBucket: "disney-plus-5b92e.appspot.com",
+    messagingSenderId: "697521935909",
+    appId: "1:697521935909:web:f125c5590f54bb55a7d1cc",
+    measurementId: "G-46YL2BHWPR"
   };
-  
 
     const firebaseApp = firebase.initializeApp(firebaseConfig);
     const db = firebaseApp.firestore();
