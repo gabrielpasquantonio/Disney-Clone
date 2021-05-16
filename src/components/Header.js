3a33bcd5e6cbf218a0fae68d0444a84ada904e8e
gabrielpasquantonio <64622616+gabrielpasquantonio@users.noreply.github.com>
2021-05-16 01:06:23 -0300

    adding components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
+import { useEffect } from "react";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { auth, provider } from "../firebase"; 
+import { auth, provider } from "../firebase";
 import {
   selectUserName,
   selectUserPhoto,
@@ -13,17 +14,36 @@ const Header = (props) => {
   const history = useHistory();
   const userName = useSelector(selectUserName);
   const userPhoto = useSelector(selectUserPhoto);
-  const handleAuth = () => {
-    auth
-      .signInWithPopup(provider)
-      .then((result) => {
-        setUser(result.user);
-      })
-      .catch((error) => {
-        alert(error.message);
-      });
-  };
 
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        setUser(user);
+        history.push("/home");
+      }
+    });
+  }, [userName]);
+
+  const handleAuth = () => {
+    if (!userName) {
+      auth
+        .signInWithPopup(provider)
+        .then((result) => {
+          setUser(result.user);
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    } else if (userName) {
+      auth
+        .signOut()
+        .then(() => {
+          dispatch(setSignOutState());
+          history.push("/");
+        })
+        .catch((err) => alert(err.message));
+    }
+  };
   const setUser = (user) => {
     dispatch(
       setUserLoginDetails({
@@ -33,7 +53,6 @@ const Header = (props) => {
       })
     );
   };
-
 
   return (
     <Nav>
@@ -70,9 +89,14 @@ const Header = (props) => {
               <img src="/images/series-icon.svg" alt="SERIES" />
               <span>SERIES</span>
             </a>
-      </NavMenu>
-      <UserImg src={userPhoto} alt={userName} />
-      </>
+          </NavMenu>
+          <SignOut>
+            <UserImg src={userPhoto} alt={userName} />
+            <DropDown>
+              <span onClick={handleAuth}>Sign out</span>
+            </DropDown>
+          </SignOut>
+        </>
       )}
     </Nav>
   );
@@ -183,6 +207,44 @@ const Login = styled.a`
 
 const UserImg = styled.img`
   height: 100%;
+`;
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+`;
+
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+
+  ${UserImg} {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+  }
+
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
 `;
 
 export default Header;
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
 import firebase from "firebase";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCbxRpjno7lkXoVIXLHy4h3bvyDU_xIELA",
     authDomain: "disneyplus-clone-79841.firebaseapp.com",
+    databaseURL: "https://disneyplus-clone-79841-default-rtdb.firebaseio.com",
     projectId: "disneyplus-clone-79841",
     storageBucket: "disneyplus-clone-79841.appspot.com",
     messagingSenderId: "249201755674",
     appId: "1:249201755674:web:5bb9fed4fbbb58ef2df3a3",
     measurementId: "G-S479KYDXXW"
-    };
-
+  };
+  
 
     const firebaseApp = firebase.initializeApp(firebaseConfig);
     const db = firebaseApp.firestore();
